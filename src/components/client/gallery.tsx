e1f12595cf6bd06dbdf61bae706ee9ff1af7cc37
divyam gupta <divyam7091@gmail.com>
2024-04-21 23:32:50 +0530

    fix hero mobile gallery

--- src/components/client/gallery.tsx
+++ src/components/client/gallery.tsx
@@ -1,69 +1,83 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TinderCard from "react-tinder-card";
+import { nanoid } from "nanoid";
 
 export function HeroMobileGallery({
   dataUrls,
 }: {
   dataUrls: Map<string, { url: string; dataUrl: string }>;
 }) {
-  const initialImages = () => Array.from(dataUrls.keys());
-  const [images, setImages] = useState(initialImages);
-  const onSwipeHandler = () => {
-    const allImages = [...images];
-    if (allImages.length !== 0) {
-      const last = allImages.pop() as string;
-      setImages(allImages);
-    } else {
-      setImages((prev) => initialImages());
+  const images = Array.from(dataUrls.keys()).map((_) => ({
+    image: _,
+    id: nanoid(5),
+  }));
+  const [swipeCards, setSwipeCards] = useState(images.slice(0, 3));
+  const [index, setIndex] = useState(3);
+  const [current, setCurrent] = useState(0);
+  const swipeHandler = (id: string, idx: number) => {
+    setCurrent((prev) => (prev + 1) % 20);
+    const card = swipeCards.find((card) => card?.id === id)!;
+    const clonedCard: (typeof images)[number] = {
+      image: card.image,
+      id: nanoid(5),
+    };
+    const oldCards = [...swipeCards];
+    const newCards = [...oldCards.filter((c) => c.id !== id), clonedCard];
+    if (index < images.length) {
+      const newCard = images.slice(index, index + 1)[0];
+      setIndex((prev) => prev + 1);
+      newCards.splice(2, 0, newCard);
     }
+    setSwipeCards(newCards);
   };
-  useEffect(() => {
-    if (images.length === 0) {
-      setImages(initialImages());
-    }
-  }, [images]);
+
   return (
     <section
       id="hero-mobile-gallery"
-      className="md:hidden h-screen relative flex justify-center snap-start pt-8"
+      className="h-screen w-80 mx-auto items-center snap-start pt-2"
     >
-      <div className=" relative">
-        <div className=" absolute top-4 left-0 right-0 h-8 bg-primary opacity-20 blur-xl"></div>
+      <div className="relative">
+        <div className=" absolute top-4 left-0 right-0 h-8 bg-primary opacity-30 blur-xl"></div>
         <h2 className=" text-3xl font-bold">Architectural Delights</h2>
       </div>
-      {images.map((_, i) => (
-        <TinderCard
-          key={_}
-          flickOnSwipe={true}
-          swipeRequirementType="position"
-          swipeThreshold={80}
-          className="absolute top-24"
-          onSwipe={(direction) => onSwipeHandler()}
-          onCardLeftScreen={(direction) => onSwipeHandler()}
-        >
+      <div className=" pt-4 relative h-[calc(100vh-300px)]">
+        {swipeCards.map((i, idx) => (
           <div
-            className="sm:w-[512px] w-[320px] mx-4  rounded-lg border-2 border-secondary overflow-hidden shadow-md shadow-secondary"
+            key={i.id}
+            className=" absolute"
             style={{
-              zIndex: images.length - i,
-              height: "calc(100vh - 350px)",
+              zIndex: images.length * 10 - idx * 10,
             }}
           >
-            <Image
-              src={dataUrls.get(_)?.url!}
-              alt="gallery image"
-              width={1500}
-              height={1500}
-              className=" w-full h-full scale-105 rounded-lg"
-              objectFit="cover"
-              placeholder="blur"
-              blurDataURL={dataUrls.get(_)?.dataUrl!}
-              loading="lazy"
-            />
+            <TinderCard
+              onSwipe={(direction) => {
+                swipeHandler(i.id, idx);
+              }}
+              preventSwipe={["top", "bottom"]}
+            >
+              <div className=" w-80 h-[calc(100vh-300px)] bg-card rounded-lg flex items-center justify-center cursor-pointer">
+                <Image
+                  fill
+                  alt="jkldfjd"
+                  src={dataUrls.get(i.image)?.url!}
+                  className=" rounded-lg"
+                />
+              </div>
+            </TinderCard>
           </div>
-        </TinderCard>
-      ))}
+        ))}
+        <div className=" flex items-center gap-[6px] absolute -bottom-10 left-1/2 -translate-x-1/2">
+          {images.map((_, i) => (
+            <div
+              className={`w-2 h-2 rounded-full ${
+                current === i ? "bg-primary" : " bg-white"
+              }`}
+            ></div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
